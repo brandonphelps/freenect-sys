a483e499a701d3a64de6a0bd1b1a7922a7706344
brandon phelps <brandonphelps@mst.edu>
2023-02-11 19:44:09 +0000

    adjusted build steps to call bind gen.

--- build.rs
+++ build.rs
@@ -1,28 +1,25 @@
 use cmake;
+use std::env;
+use std::path::PathBuf;
 
 fn main() {
-
     let dst = cmake::build("libfreenect");
-
 
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
     println!("cargo:rustc-link-search={}", "/usr/lib/x86_64-linux-gnu");
-
-    // println!("cargo:rustc-link-search=natiusb-1
     println!("cargo:rustc-link-lib=static=freenect");
-
     println!("cargo:rustc-link-lib=static=usb-1.0");
     println!("cargo:rustc-link-lib=dylib=udev");
 
-    // let bindings = bindgen::Builder::default()
-    //     .header("wrapper.h")
-    //     .clang_arg("-I/libfreenect/include")
-    //     .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-    //     .generate()
-    //     .expect("unable to generate bindings");
+    let bindings = bindgen::Builder::default()
+        .header("libfreenect/include/libfreenect.h")
+        .clang_arg("-I./libfreenect/include")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("unable to generate bindings");
 
-    // let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    // bindings.write_to_file(out_path.join("bindings.rs"))
-    //     .expect("Couldn't write bindings");
+    bindings.write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings");
 }
